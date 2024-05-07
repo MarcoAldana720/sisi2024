c20ca422df115db9cd3c954391b7537b0b34e860
FrankSnchz <114689030+FrankSnchz@users.noreply.github.com>
2024-05-07 16:46:20 -0600

    weather & maps commit

--- index.php
+++ index.php
@@ -11,7 +11,6 @@
     <!--CSS-->
     <link rel="preload" href="css/styles.css" as="style">
     <link rel="stylesheet" href="css/styles.css">
-    <link rel="stylesheet" href="css/style-clima.css">
 
 </head>
 <body>
@@ -19,69 +18,66 @@
     <?php
         include "head.php";
     ?>
-    <div id="contenedor">
-        <div id="caja1">
-            <p class="cli" id="temperatura-valor"></p>
-            <p class="cli" id="temperatura-descripcion"></p>
-        </div>
-        <div id="caja2">
-            <p class="cli" id="ubicacion"></p> 
-        </div>
-        <div id="caja3">
-            <p class="cli" >Veloc. del Viento</p>
-            <p class="cli" id="viento-velocidad"></p>
-        </div>
-    </div>
-    <script src="app.js"></script>
-    <section class="marginLogin">
-      <h1>Registro al SISI 2024</h1>
-      <!-- MANDAR UN REGISTRO EN LA BD -->
-      <form action="registrar.php" method="POST">
-            <div class="form-group">
-              <label for="exampleFormControlInput1">Nombre completo: </label>
-              <input type="text" class="form-control" placeholder="Escriba su nombre" id="reg_nom" name="reg_nom" required>
-            </div>
-            <div class="form-group">
-                <label for="exampleFormControlInput1">Matrícula: </label>
-                <input type="text" class="form-control" placeholder="Ejemplo: E12345678" id="reg_mat" name="reg_mat" required>
-            </div>
-            <div class="form-group">
-                <label for="exampleFormControlInput1">Contraseña: </label>
-                <input type="password" class="form-control" placeholder="Ingrese su contraseña" id="reg_con" name="reg_con" required>
-            </div>
-            <div class="form-group">
-                <label for="exampleFormControlInput1">Correo institucional: </label>
-                <input type="email" class="form-control" placeholder="ejemplo@merida.tecnm.mx" id="reg_cor" name="reg_cor" required>
+    
+    <div>
+      <?php
+          include "clima.php";
+      ?>
+      <section class="marginLogin">
+        <h1>Registro al SISI 2024</h1>
+        <!-- MANDAR UN REGISTRO EN LA BD -->
+        <form action="registrar.php" method="POST">
+              <div class="form-group">
+                <label for="exampleFormControlInput1">Nombre completo: </label>
+                <input type="text" class="form-control" placeholder="Escriba su nombre" id="reg_nom" name="reg_nom" required>
               </div>
               <div class="form-group">
-                <label for="exampleFormControlInput1">Celular: </label>
-                <input type="tel" class="form-control" placeholder="Ejemplo: 9991234567" id="reg_cel" name="reg_cel" required>
+                  <label for="exampleFormControlInput1">Matrícula: </label>
+                  <input type="text" class="form-control" placeholder="Ejemplo: E12345678" id="reg_mat" name="reg_mat" required>
               </div>
               <div class="form-group">
-                <label for="exampleFormControlSelect1">Semestre: </label>
-                <select class="form-control" id="reg_sem" name="reg_sem">
-                  <option value="" selected disabled>Seleccione una opcion</option>
-                  <option value="1">Primer semestre</option>
-                  <option value="2">Segundo semestre</option>
-                  <option value="3">Tercer semestre</option>
-                  <option value="4">Cuarto semestre</option>
-                  <option value="5">Quinto semestre</option>
-                  <option value="6">Sexto semestre</option>
-                  <option value="7">Septimo semestre</option>
-                  <option value="8">Octavo semestre</option>
-                  <option value="9">Noveno semestre</option>
-                  <option value="10">Decimo semestre</option>
-                  <option value="11">Onceavo semestre</option>
-                  <option value="12">Doceavo semestre</option>
-                  <option value="13">Treceavo semestre</option>
-                </select>
+                  <label for="exampleFormControlInput1">Contraseña: </label>
+                  <input type="password" class="form-control" placeholder="Ingrese su contraseña" id="reg_con" name="reg_con" required>
               </div>
-              <div class="btn_center">
-                <button type="submit" class="btn-primary">Registrarse</button>
-                <a href="login.php" class="a_login">Iniciar sesión</a>
-              </div>
+              <div class="form-group">
+                  <label for="exampleFormControlInput1">Correo institucional: </label>
+                  <input type="email" class="form-control" placeholder="ejemplo@merida.tecnm.mx" id="reg_cor" name="reg_cor" required>
+                </div>
+                <div class="form-group">
+                  <label for="exampleFormControlInput1">Celular: </label>
+                  <input type="tel" class="form-control" placeholder="Ejemplo: 9991234567" id="reg_cel" name="reg_cel" required>
+                </div>
+                <div class="form-group">
+                  <label for="exampleFormControlSelect1">Semestre: </label>
+                  <select class="form-control" id="reg_sem" name="reg_sem">
+                    <option value="" selected disabled>Seleccione una opcion</option>
+                    <option value="1">Primer semestre</option>
+                    <option value="2">Segundo semestre</option>
+                    <option value="3">Tercer semestre</option>
+                    <option value="4">Cuarto semestre</option>
+                    <option value="5">Quinto semestre</option>
+                    <option value="6">Sexto semestre</option>
+                    <option value="7">Septimo semestre</option>
+                    <option value="8">Octavo semestre</option>
+                    <option value="9">Noveno semestre</option>
+                    <option value="10">Decimo semestre</option>
+                    <option value="11">Onceavo semestre</option>
+                    <option value="12">Doceavo semestre</option>
+                    <option value="13">Treceavo semestre</option>
+                  </select>
+                </div>
+                <div class="btn_center">
+                  <button type="submit" class="btn-primary">Registrarse</button>
+                  <a href="login.php" class="a_login">Iniciar sesión</a>
+                </div>
         </form>
-    </section>
+      </section>
+
+      <?php
+          include "maps.php";
+      ?>
+    </div>
+
 
     <?php
         include "tail.php";
--- login.php
+++ login.php
@@ -13,39 +13,49 @@
     <?php
         include "head.php";
     ?>
+    <div>
+        <?php
+            include "clima.php";
+        ?>
 
-    <section class="marginLogin">
-        <div class="banner">
-            <img src="img/itm logo.png" class="imagen1" alt="Banner SISI 2024">
-        </div>
+        <section class="marginLogin">
+            <div class="banner">
+                <img src="img/itm logo.png" class="imagen1" alt="Banner SISI 2024">
+            </div>
 
-        <h1>Inicio de sesión</h1>
+            <h1>Inicio de sesión</h1>
 
-        <form action="autentifico.php" method="POST">
-            <div>
-                <label for="exampleFormControlInput1">Usuario: </label>
-                <input type="text" class="form-control margen" placeholder="Matrícula" id="matricula" name="matricula" required>
-            </div>
-            <div>
-                <label for="exampleFormControlInput1">Contraseña: </label>
-                <input type="password" class="form-control margen" placeholder="Ingrese su contraseña" id="contrasena" name="contrasena" required>
-            </div>
-            <div class="btn_center">
-                <button type="submit" class="btn-primary">Entrar</button>
-            </div>
-            <!-- INICIO: Alerta De Autentificacion -->
-            <?php
-                $errorusuario = isset($_GET["errorusuario"]);
-                if($errorusuario == "SI") {
-                    echo '<p id="alerta">usuario y/o contraseña incorrecta</p>';
-                }
-            ?>
-            <!-- FINAL: Alerta De Autentificacion -->
-            <div>
-                <p>¿Aún no tienes una cuenta? <a href="index.php" class="regis">Regístrate</a></p>
-            </div>
-        </form>
-    </section>
+            <form action="autentifico.php" method="POST">
+                <div>
+                    <label for="exampleFormControlInput1">Usuario: </label>
+                    <input type="text" class="form-control margen" placeholder="Matrícula" id="matricula" name="matricula" required>
+                </div>
+                <div>
+                    <label for="exampleFormControlInput1">Contraseña: </label>
+                    <input type="password" class="form-control margen" placeholder="Ingrese su contraseña" id="contrasena" name="contrasena" required>
+                </div>
+                <div class="btn_center">
+                    <button type="submit" class="btn-primary">Entrar</button>
+                </div>
+                <!-- INICIO: Alerta De Autentificacion -->
+                <?php
+                    $errorusuario = isset($_GET["errorusuario"]);
+                    if($errorusuario == "SI") {
+                        echo '<p id="alerta">usuario y/o contraseña incorrecta</p>';
+                    }
+                ?>
+                <!-- FINAL: Alerta De Autentificacion -->
+                <div>
+                    <p>¿Aún no tienes una cuenta? <a href="index.php" class="regis">Regístrate</a></p>
+                </div>
+            </form>
+        </section>
+
+        <?php
+          include "maps.php";
+        ?>
+    </div>
+    
 
     <?php
         include "tail.php";
